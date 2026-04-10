c81181642f90c23c1a4553abba67612cc441b9a4
dkujku21 <korbidalinpf@gmail.com>
2026-04-10 12:05:23 +0200

    Implement user management with add and delete functions

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+let users = ["Ardit", "Elira", "Sara"];
 
+function renderUsers() {
+    let list = document.getElementById("userList");
+    list.innerHTML = "";
+
+    users.forEach((user, index) => {
+        let li = document.createElement("li");
+        li.textContent = user;
+        let btn = document.createElement("button");
+        btn.textContent = "Delete";
+        btn.onclick = () => deleteUser(index);
+
+        li.appendChild(btn);
+        list.appendChild(li);
+    });
+}
+
+function addUser() {
+    let input = document.getElementById("username");
+
+    if (input.value === "") {
+        alert("Write a name!");
+        return;
+    }
+
+    users.push(input.value);
+    input.value = "";
+    renderUsers();
+}
+function deleteUser(index) {
+    users.splice(index, 1);
+    renderUsers();
+}
+
+renderUsers();
